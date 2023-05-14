2b2df048cc42429cec26160bd5abd54b38a9c7d1
Matutozi <sobowalegz2@gmail.com>
2023-05-14 22:48:07 +0100

    function that will print all the environment variables

---
+++ builtin_env.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_environment - function that prints the environment variables
+ *
+ * Return: return void
+*/
+
+void print_environment()
+{
+	char **env = environ;
+
+	while (*env)
+	{
+		write(STDOUT_FILENO, *env, _strlen(*env));
+		write(STDOUT_FILENO, "\n", 1);
+		env++
+	}
+}
+
