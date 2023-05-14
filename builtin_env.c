6b08474545e4573dd9f5342bafe9b5e5ac84fd81
Matutozi <sobowalegz2@gmail.com>
2023-05-14 22:56:49 +0100

    error handling

--- builtin_env.c
+++ builtin_env.c
@@ -8,13 +8,14 @@
 
 void print_environment()
 {
+	extern char **environ;
 	char **env = environ;
 
 	while (*env)
 	{
 		write(STDOUT_FILENO, *env, _strlen(*env));
 		write(STDOUT_FILENO, "\n", 1);
-		env++
+		env++;
 	}
 }
 
