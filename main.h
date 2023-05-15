a89938a69883ce5f89d32f4f906661c5f8184489
Matutozi <sobowalegz2@gmail.com>
2023-05-15 15:18:26 +0100

    added the prototype for the env function

--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #include <unistd.h>
 
 #define BUFFERSIZE 1024
+extern char **environ;
 
 ssize_t prompt(char *chr);
 char *_strdup(char *str);
@@ -24,6 +25,6 @@ char *find_path(char *command);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void assign_line(char **lineptr, size_t *n, char *buffer, size_t j);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
+void print_environment(void);
 #endif /*MAIN_H */
 
