9839445dbf74093a2455f7d63f9727b00a7ff411
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-05-15 04:55:24 -0700

    exec.c

--- exec.c
+++ exec.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * parse_cmd - Parses a command string into an array of arguments.
- * @cmd: Pointer to the command string to parse.
+ * execute_cmd - Executes a command.
+ * @cmd: Pointer to the command string.
  *
- * Return: Pointer to an array of pointers to the arguments,
- * or NULL on failure.
+ * Return: Always returns 0.
  */
 
-char **parse_cmd(char *cmd)
+int execute_cmd(char *cmd)
 {
 	char *cmd_cpy = _strdup(cmd);
 	char *token = NULL, *delim = " \n";
-	int argc = 0;
+	int status, argc = 0;
 	char **argv = malloc(256 * sizeof(char *));
 
 	token = _strtok(cmd_cpy, delim);
@@ -22,32 +21,15 @@ char **parse_cmd(char *cmd)
 		argc++;
 	}
 	argv[argc] = NULL;
-	free(cmd_cpy);
-
-	return (argv);
-}
-
-/**
- * execute_cmd - Executes a command.
- * @cmd: Pointer to the command string.
- *
- * Return: Always returns 0.
- */
-
-int execute_cmd(char *cmd)
-{
-	char **argv = parse_cmd(cmd);
-	int status;
 
 	if (_strcmp(argv[0], "exit") == 0)
 	{
-		free(argv);
+		free(cmd_cpy), free(argv);
 		exit(0);
 	}
 	if (fork() == 0)
 	{
 		char *full_path = find_path(argv[0]);
-
 		if (full_path)
 		{
 			if (execve(full_path, argv, NULL) == -1)
